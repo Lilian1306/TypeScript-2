fc8b0c2d979bafd935c66e7d05e1e7eea2b09d89
DaniM0613 <wwh.rj.92@gmail.com>
2024-10-07 15:14:36 -0600

    Definiendo acciones en el Reducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,8 +1,7 @@
 import type { Activity } from "../types"
 
-export type ActivityActions = {
-
-}
+export type ActivityActions = 
+{ type: 'save-activity', payload: { newActivity : Activity}  }
 
 type ActivityState = {
     activities : Activity[]
@@ -16,5 +15,8 @@ export const activityReducer = (
     state : ActivityState = initialState,
     action: ActivityActions
 ) => {
-
+    if(action.type === 'save-activity'){
+        // Este codigo maneja la logica para actualizar el state
+        console.log('desde el type de save-activity')
+    }
 }
